e0bac4b859ef7dfc8a7366b0b48536ffcf55ba1e
Bokis Angelov <bokisangelov@gmail.com>
2025-04-07 16:59:16 +0300

    Add lang switcher

--- main/templatetags/language_switcher.py
+++ main/templatetags/language_switcher.py
@@ -1,15 +1,19 @@
 from django import template
 from django.utils.translation import get_language
 from django.conf import settings
+import logging
 
 register = template.Library()
+logger = logging.getLogger(__name__)
 
 @register.simple_tag
 def available_languages():
-    print("Available languages:", settings.LANGUAGES)
+    # Log the available languages instead of printing
+    logger.debug("Available languages: %s", settings.LANGUAGES)
     return settings.LANGUAGES
 
 @register.simple_tag
 def current_language():
-    print("Current language:", get_language())
+    # Log the current language
+    logger.debug("Current language: %s", get_language())
     return get_language()
